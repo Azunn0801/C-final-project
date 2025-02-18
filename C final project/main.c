73f082a164bfa5ea7be020967bf021f8655bb0d2
Azunn0801 <125119856+Azunn0801@users.noreply.github.com>
2025-02-18 18:58:41 +0700

    Update main.c

--- C final project/main.c
+++ C final project/main.c
@@ -8,9 +8,9 @@ int main(void) {
     load_file("data/category.bin", "rb");
     load_file("data/product.bin", "rb");
     Sleep(2000);
-    //system("cls");
+    system("cls");
 
-    //log_in_menu();
+    log_in_menu();
     entry_menu();
     return 0;
 }
